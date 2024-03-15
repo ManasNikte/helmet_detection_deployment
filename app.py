724e03e7e6c0d11ef44cb18a4a96db4e23e681b5
Janak Rajpurohit <115173636+Janak-Rajpurohit@users.noreply.github.com>
2024-03-15 19:35:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def video():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 #if __name__ == '__main__':
-app.run(debug=False)
+app.run(debug=False,host='0.0.0.0')
 
